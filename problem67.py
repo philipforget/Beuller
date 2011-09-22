7a236fbcb1dd8909b1425b9bd1bd7dcfcf3e6f83
Philip Forget <philipforget@gmail.com>
2011-09-22 09:26:39 -0400

    First commit

---
+++ problem67.py
@@ -0,0 +1,8 @@
+def flatten_triangl(t):
+    while len(t) > 1:
+        t[-2] = [max(p + t[-1][i], p + t[-1][i + 1]) for i, p in enumerate(t[-2])]
+        del t[-1]
+    return t
+
+with open('problem67_triangles.txt', 'r') as triangle_file:
+    print(flatten_triangl([[int(entry) for entry in line.split(' ')] for line in triangle_file])[0][0])
