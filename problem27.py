58d2ee8af194d54f90694ade20c056bf66942325
Philip Forget <philipforget@gmail.com>
2011-09-25 21:55:30 -0400

    For kevin

---
+++ problem27.py
@@ -0,0 +1,28 @@
+__problem__ = """
+Starting with the number 1 and moving to the right in a clockwise direction a 5
+by 5 spiral is formed as follows:
+
+21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed
+in the same way?
+
+1.3.5.7.9...13...17...21...25.....31.....37.....43
+"""
+
+
+class Main():
+    def __init__(self):
+        rows = 5
+        print [j for j in range(1, rows**2 + 1)[::2]]
+        print [j for j in range(1, rows**2 + 1)[::4]]
+
+
+if __name__ == '__main__':
+    Main()
